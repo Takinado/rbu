9310545ab0883bc396b9a4eb97e84c62e1c689fe
Takinado <takinado@mail.ru>
2017-08-21 14:08:30 +0300

    Add Celery and Redis

---
+++ rbu/celery.py
@@ -0,0 +1,43 @@
+# https://www.codingforentrepreneurs.com/blog/celery-redis-django/
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rbu.settings')
+
+app = Celery('rbu')
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+# http://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html
+app.conf.beat_schedule = {
+
+    'add-every-5-seconds': {
+        'task': 'task_import_csv',
+        'schedule': 5.0
+    },
+
+}
+
+# for debug
+# celery -A rbu worker -B -l info
+
+# for production
+# celery -A rbu worker -l info
+# celery -A rbu beat -l info
+# redis-server
+
+
---
+++ report/tasks.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+import random
+from rbu.celery import app
+from report.views import import_one_csv
+
+from rbu import settings
+
+
+@app.task(name="sum_two_numbers")
+def add(x, y):
+    print(x + y)
+    return x + y
+
+
+@app.task(name="multiply_two_numbers")
+def mul(x, y):
+    total = x * (y * random.randint(3, 100))
+    return total
+
+
+@app.task(name="sum_list_numbers")
+def xsum(numbers):
+    return sum(numbers)
+
+
+@app.task(name="task_import_csv")
+def task_import():
+    import_one_csv()
+    return True
+
