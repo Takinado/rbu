f44500434f58d7c8e66681c6de7793331fbb0940
Takinado <takinado@mail.ru>
2017-09-13 01:18:10 +0300

    Страница UnloadDetailView работает

--- unloads/urls.py
+++ unloads/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from unloads.views import unload_list_view
+from unloads.views import unload_list_view, UnloadDetailView
 
 urlpatterns = [
     url(r'^$', unload_list_view, name="unload_list"),
+    url(r'^(?P<pk>\d+)/$', UnloadDetailView.as_view(), name="unload_detail"),
     # url(r'^status_list/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
     #     StatusDayView.as_view(),
     #     name="status_list_view"),
 ]
-
