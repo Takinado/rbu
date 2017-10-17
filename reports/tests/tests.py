f4353152562685171a8f4d5009184d80dc91ed19
Takinado <takinado@mail.ru>
2017-10-17 17:22:14 +0300

    Исправлена ошибка отображения общей суммы цемента за месяц

--- reports/tests/tests.py
+++ reports/tests/tests.py
@@ -1,4 +1,4 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 from django.test import LiveServerTestCase
 
@@ -14,14 +14,18 @@ class ReportTestCase(LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(2)
 
-        cement1 = Cement.objects.create_cement(date.today(), 100)
+        now_date = date.today()
+
+        cement1 = Cement.objects.create_cement(now_date, 100)
+        cement2 = Cement.objects.create_cement(now_date - timedelta(days=1), 200)
+        cement3 = Cement.objects.create_cement(now_date.replace(month=now_date.month-1), 150)
 
     def find_search_results(self):
         return self.browser.find_elements_by_css_selector('.cement-li')
 
     def test_view_report(self):
         """
-        Тест что пользователь может просматривать статусы
+        Тест что пользователь может просматривать отчет по цементу
         :return:
         """
 
@@ -36,39 +40,22 @@ class ReportTestCase(LiveServerTestCase):
 
         # На странице список Цементов за текущий месяц
         search_results = self.find_search_results()
-        self.assertGreaterEqual(len(search_results), 1)
+        self.assertGreaterEqual(len(search_results), 2)
 
-        # # Можно выбрать дату и отправить запрос
-        # date_input.send_keys('07.08.2017')
-        # self.browser.find_element_by_css_selector('form button').click()
-        #
-        # # И увидеть результат... статусы за дату
-        # search_results = self.find_search_results()
-        # self.assertEqual(len(search_results), 2)
-        #
-        # # Можно выбрать статус
-        # search_results[0].click()
-        #
-        # # На странице отгрузки увидеть дату, время и все данные из модели
-        # # Числом статусов: общим, с ошибками, с предупреждениями, не посчитаных, без отгрузки
-        # # а также модель фото и если нужно фото
-        # # ссылку на отгрузку
-        # date_url = datetime.strftime(datetime.now(), "%Y/%m/%d")
-        #
-        # self.assertEqual(
-        #     self.browser.current_url,
-        #     '{}/status/3/'.format(self.live_server_url)
-        # )
-        #
-        # try:
-        #     brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
-        # except NoSuchElementException:
-        #     return self.fail('Не открывается страница Статуса. На странице не найден элемент ".navbar-brand"')
-        #
-        # self.assertEqual(
-        #     self.browser.find_element_by_css_selector('#status-date').text,
-        #     '07.08.2017'
-        # )
+        # Совпадает кол-во дней за месяц на странице и в тотал
+        total_day = self.browser.find_element_by_css_selector('#total-day').text
+        self.assertEqual(len(search_results), int(total_day))
+
+        # Совпадает сумма цемента за месяц на странице и в тотал
+        total_value = self.browser.find_element_by_css_selector('#total-value').text
+        cement_values_tag = self.browser.find_elements_by_css_selector('.cement-value')
+        cement_values = [val.text for val in cement_values_tag]
+        self.assertEqual(int(total_value), sum(map(int, cement_values)))
+
+        # есть ссылка и можно перейти на прошлый месяц
+
+        # c прошлого есть ссылка и можно перейти снова на текущий
+
 
         self.fail('Весь тест пока не выполняется')
 
--- reports/views.py
+++ reports/views.py
@@ -23,8 +23,11 @@ class CementMonthArchiveView(MonthArchiveView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(CementMonthArchiveView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['total'] = Cement.objects.filter().aggregate(Sum('value'))
+        # Add in a QuerySet of all
+        context['total'] = Cement.objects.filter(
+            date__year=self.get_year(),
+            date__month=self.get_month(),
+        ).aggregate(Sum('value'))
         return context
 
     # Если необходимо обработать какие-то дополнительные параметры из url, то у DetailView(как и у ListView)
